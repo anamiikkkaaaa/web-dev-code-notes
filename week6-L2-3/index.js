056253bfc846a54e5cfe6d1eb3ba0274c869cf33
Anamika <anamikarajput18564@gmail.com>
2025-10-22 21:31:13 +0530

    till week6

---
+++ week6-L2-3/index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "analovesbroccoli";
+const app = express();
+
+app.use(express.json());
+
+function auth(req, res, next){
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET);
+    if(decodedData.username){
+        req.username = decodedData.username;
+        next();
+    }
+    else{
+        res.json({
+            message: "you are not logged in"
+        })
+    }
+}
+let users = [];
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+})
+app.post("/signup", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    });
+
+    res.json({
+        message: "you have signed up"
+    })
+
+});
+
+app.post("/signin", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+
+    for(let i=0; i<users.length; i++){
+        if(users[i].username == username && users[i].password == password){
+            foundUser = users[i];
+        }
+    }
+
+    if(foundUser){
+        const token = jwt.sign({
+            username
+        }, JWT_SECRET);
+        foundUser.token = token;
+        res.json({
+            token: token
+        });
+    }
+    else{
+        res.json({
+            message: "invalid credentials"
+        });
+    }
+
+    console.log(users);
+});
+
+app.get("/me", auth, (req, res) => {
+        let currUser = req.username;
+        let foundUser = null;
+
+        for(let i=0; i<users.length; i++){
+            if(users[i].username == currUser){
+                foundUser = users[i];
+            }
+        }
+
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        }) 
+    });
+
+app.listen(3000);
